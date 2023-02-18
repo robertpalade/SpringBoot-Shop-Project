d3315f031153e787b965fdb95f0bb6ecd4b986bc
robertpalade <palade.robert99@gmail.com>
2023-02-18 13:05:11 +0200

    first commit

---
+++ src/main/java/com/shop/project/springbootshop/SpringBootShopApplication.java
@@ -0,0 +1,13 @@
+package com.shop.project.springbootshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shop/project/springbootshop/SpringBootShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.project.springbootshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
